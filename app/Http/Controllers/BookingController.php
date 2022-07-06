b42246d39933b0701c1f1f211ab6e8efc0da030a
hamza umar <humar6078@gmail.com>
2022-07-06 17:46:10 +0000

    service endtime

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Bookings\TimeSlotGenerator;
 use App\Models\Schedule;
+use App\Models\Service;
 use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 
@@ -14,8 +15,9 @@ class BookingController extends Controller
         // 9:00 5:00
 
         $schedule = Schedule::find(1);
+        $service = Service::find(1);
         // dd($schedule->date);
-        $slots = (new TimeSlotGenerator($schedule))->get();
+        $slots = (new TimeSlotGenerator($schedule, $service))->get();
 
         return view('bookings.create', [
             'slots' => $slots
