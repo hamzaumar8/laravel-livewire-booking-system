68df9caccbe89e60f35618bb07e21e04e6eb347a
hamza umar <humar6078@gmail.com>
2022-07-07 00:31:16 +0000

    scaffolding livewire component

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Bookings\Filters\SlotsPassedTodayFilter;
-use App\Bookings\Filters\UnavailabilityFilter;
-use App\Bookings\TimeSlotGenerator;
-use App\Models\Appointment;
 use App\Models\Employee;
 use App\Models\Schedule;
 use App\Models\Service;
-use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
---
+++ app/Http/Livewire/CreateBooking.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Employee;
+use App\Models\Service;
+use Livewire\Component;
+
+class CreateBooking extends Component
+{
+    public $employees;
+
+    public $state = [
+        'service' => '',
+        'employee' => '',
+        'time' => '',
+        'name' => '',
+        'email' => '',
+    ];
+
+    public function mount()
+    {
+        $this->employees = collect();
+    }
+
+    public function updatedStateService($serviceId)
+    {
+        $this->state['employee'] = '';
+
+        if (!$serviceId) {
+            $this->employees = collect();
+            return;
+        }
+
+        $this->clearTime();
+
+        $this->employees = $this->selectedService->employees;
+    }
+
+    public function getSelectedServiceProperty()
+    {
+        if (!$this->state['service']) {
+            return null;
+        }
+
+        return Service::find($this->state['service']);
+    }
+
+    public function getSelectedEmployeeProperty()
+    {
+        if (!$this->state['employee']) {
+            return null;
+        }
+
+        return Employee::find($this->state['employee']);
+    }
+
+    public function render()
+    {
+        $services = Service::get();
+        return view('livewire.create-booking', [
+            'services' => $services
+        ])->layout('layouts.guest');
+    }
+}
